bd5271e5b5aada47897a0f0df245761cb30ec827
akshaya <akshaya.srinivas@wob.ag>
2021-10-07 19:45:47 +0200

    day 2

---
+++ day-2/main.js
@@ -0,0 +1,54 @@
+import './style.css'
+
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const squares = document.querySelectorAll('.square')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+  currentActive++
+
+  if(currentActive>squares.length) {
+    currentActive = squares.length
+  }
+
+update()
+})
+
+prev.addEventListener('click', () => {
+  currentActive--
+
+
+  if(currentActive<1) {
+    currentActive = 1
+  }
+
+  update()
+})
+
+function update() {
+  squares.forEach((square, idx) => {
+    if(idx < currentActive) {
+      square.classList.add('active')
+    } else {
+      square.classList.remove('active')
+    }
+  })
+
+  const actives = document.querySelectorAll('.active')
+
+  progress.style.width = (actives.length - 1) / (squares.length - 1) * 100 + '%'
+    
+
+  if(currentActive === 1) {
+    prev.disabled = true
+  }
+  else if(currentActive === squares.length) {
+    next.disabled = true
+  } else {
+    prev.disabled = false
+    next.disabled = false
+  }
+}
