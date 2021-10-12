79e7c83e74447316dd24bc501ac8e1142fe0114c
akshaya <akshaya.srinivas@wob.ag>
2021-10-13 00:13:14 +0200

    day-7

---
+++ day-7/main.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+const sounds = ['applause', 'boo', 'gasp', 'victory']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
