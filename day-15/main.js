316c8e2ddf42a2e859bfad71263bf84cc680bdfb
akshaya <akshaya.srinivas@wob.ag>
2021-10-24 17:06:51 +0200

    day-15

---
+++ day-15/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
