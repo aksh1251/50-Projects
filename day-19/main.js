8e67a297dcd9eaacbac023b8b99a152d2d6257f7
akshaya <akshaya.srinivas@wob.ag>
2021-10-29 11:24:04 +0200

    day-19

---
+++ day-19/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
